5a22e497a8e190b90f4e07b82aa94b1a7d4b60e6
Nadezhda <el.shtolts@yandex.ru>
2025-02-09 22:32:28 +0300

    added assets +uicell

---
+++ FirstChallengeGameBomb/Cell/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  FirstChallengeGameBomb
+//
+//  Created by Надежда Капацина on 09.02.2025.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    static let identifier = "CategoryCell"
+}
