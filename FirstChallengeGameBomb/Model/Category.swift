a3a830207860926f979449350eed71865d75316e
Nadezhda <121130315+ElShtolts13@users.noreply.github.com>
2025-02-10 08:49:11 +0300

    added categories screen (#8)
    * added Category model
    
    * added assets +uicell
    
    * added completed categories screen
    
    * no message
    
    * update func goToHelpVC()
    
    ---------
    
    Co-authored-by: Nadezhda <el.shtolts@yandex.ru>

---
+++ FirstChallengeGameBomb/Model/Category.swift
@@ -0,0 +1,12 @@
+//
+//  CathergoryModel.swift
+//  FirstChallengeGameBomb
+//
+//  Created by Надежда Капацина on 09.02.2025.
+//
+
+struct Category {
+    let name: String
+    let imageName: String
+    var isSelected: Bool
+}
