61485c5c3737c9c71b4c27e74971bc5903b00d9f
GrigoryDushin <g.s.dushin@gmail.com>
2025-02-11 15:54:21 +0100

    without design, just reason

--- FirstChallengeGameBomb/Controller/FinalGameView.swift
+++ FirstChallengeGameBomb/Controller/FinalGameView.swift
@@ -6,7 +6,107 @@
 //
 
 import UIKit
+import Settings
 
-final class FinalGameViewController: UIViewController {
+final class FinalGameViewController: BaseViewController {
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Конец игры"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let gameImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "finalGameImage"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "В следующем раунде после каждого ответа хлопать в ладоши"
+        label.font = .systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let otherTaskButton = UIButton(
+        title: "Другое задание",
+        backgroundColor: .systemGreen
+    )
+    
+    private let restartButton = UIButton(
+        title: "Начать заново",
+        backgroundColor: .systemGreen
+    )
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [otherTaskButton, restartButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, gameImageView, descriptionLabel, buttonStackView])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 24
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        view.addSubview(mainStackView)
+        
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        otherTaskButton.translatesAutoresizingMaskIntoConstraints = false
+        restartButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            gameImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            gameImageView.heightAnchor.constraint(equalTo: gameImageView.widthAnchor),
+            
+            buttonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            
+            otherTaskButton.heightAnchor.constraint(equalToConstant: 50),
+            restartButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
+
+
+
+
+//Delete before pull
+
+extension UIButton {
+    convenience init(title: String, backgroundColor: UIColor) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = .systemFont(ofSize: 15, weight: .black)
+        self.tintColor = UIColor(named: "TextColor")
+        self.backgroundColor = backgroundColor
+        self.layer.cornerRadius = 10
+            
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
 }
