d60780e254dbf34b43be6337c52f83da1e12b267
Руслан <rus.baranovskiy@gmail.com>
2025-02-13 21:16:00 +0300

    ь

--- Modules/Settings/Sources/Settings/Helpers/TableManager/SettingsTableManager.swift
+++ Modules/Settings/Sources/Settings/Helpers/TableManager/SettingsTableManager.swift
@@ -17,18 +17,18 @@ extension SettingsTableManager: @preconcurrency ISettingsTableManager {
         tableView?.dataSource = self
         
         tableView?.register(
-            SelectionTitleTableViewCell.self,
-            forCellReuseIdentifier: SelectionTitleTableViewCell.reuseIdentifier
+            SelectionTitleTableViewSection.self,
+            forCellReuseIdentifier: SelectionTitleTableViewSection.reuseIdentifier
         )
         
         tableView?.register(
-            ToggleSettingsTableViewCell.self,
-            forCellReuseIdentifier: ToggleSettingsTableViewCell.reuseIdentifier
+            ToggleTableViewSection.self,
+            forCellReuseIdentifier: ToggleTableViewSection.reuseIdentifier
         )
         
         tableView?.register(
-            DisclosureSettingsTableViewCell.self,
-            forCellReuseIdentifier: DisclosureSettingsTableViewCell.reuseIdentifier
+            DisclosureTableViewSection.self,
+            forCellReuseIdentifier: DisclosureTableViewSection.reuseIdentifier
         )
     }
     
@@ -39,7 +39,25 @@ extension SettingsTableManager: @preconcurrency ISettingsTableManager {
 }
 
 extension SettingsTableManager: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let section = sections[safe: indexPath.section] else { return .zero }
+        switch section {
+        case .titleSelection:
+            return 183
+        case .disclosureSection:
+            return 235
+        case .toggleSection:
+            return 151
+        }
+    }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 16.0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return .init()
+    }
 }
 
 extension SettingsTableManager: UITableViewDataSource {
@@ -47,15 +65,7 @@ extension SettingsTableManager: UITableViewDataSource {
         sections.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let section = sections[safe: section] else { return .zero }
-        switch section {
-        case .titleSelection(_, let viewModels):
-            return viewModels.isEmpty ? .zero : 1
-        case .disclosureSection(let viewModels):
-            return viewModels.count
-        case .toggleSection(let viewModels):
-            return viewModels.count
-        }
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -63,22 +73,24 @@ extension SettingsTableManager: UITableViewDataSource {
         switch section {
         case .titleSelection(_, let viewModels):
             guard let cell = tableView.dequeueReusableCell(
-                    withIdentifier: SelectionTitleTableViewCell.reuseIdentifier
-                  ) as? SelectionTitleTableViewCell else { return .init() }
+                    withIdentifier: SelectionTitleTableViewSection.reuseIdentifier
+                  ) as? SelectionTitleTableViewSection else { return .init() }
             cell.fill(viewModels)
             return cell
         case .disclosureSection(let viewModels):
-            guard let viewModel = viewModels[safe: indexPath.row],
-                  let cell = tableView.dequeueReusableCell(
-                    withIdentifier: DisclosureSettingsTableViewCell.reuseIdentifier
-                  ) as? DisclosureSettingsTableViewCell else { return .init() }
-            cell.fill(viewModel)
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: DisclosureTableViewSection.reuseIdentifier
+                  ) as? DisclosureTableViewSection else { return .init() }
+            tableView.layoutIfNeeded()
+            tableView.beginUpdates()
+            cell.fill(viewModels)
+            tableView.endUpdates()
             return cell
         case .toggleSection(let viewModels):
             guard let viewModel = viewModels[safe: indexPath.row],
                   let cell = tableView.dequeueReusableCell(
-                    withIdentifier: ToggleSettingsTableViewCell.reuseIdentifier
-                  ) as? ToggleSettingsTableViewCell else { return .init() }
+                    withIdentifier: ToggleTableViewSection.reuseIdentifier
+                  ) as? ToggleTableViewSection else { return .init() }
             cell.fill(viewModel)
             return cell
         }
--- Modules/Settings/Sources/Settings/Resources/ModuleAsset.swift
+++ Modules/Settings/Sources/Settings/Resources/ModuleAsset.swift
@@ -3,4 +3,8 @@ import UIKit
 enum ModuleAsset {
     static var backgroundColor: UIColor { UIColor(named: "backgroundColor", in: .module, compatibleWith: nil)! }
     static var backgroundImage: UIImage { return UIImage(named: "backgroundImage", in: .module, with: nil)! }
+    static var cellBackgroundColor: UIColor { UIColor(named: "cellBackgroundColor", in: .module, compatibleWith: nil)! }
+    static var mainYellowColor: UIColor { UIColor(named: "mainYellowColor", in: .module, compatibleWith: nil)! }
+    static var sectionBackgroundColor: UIColor { UIColor(named: "sectionBackgroundColor", in: .module, compatibleWith: nil)! }
+    
 }
